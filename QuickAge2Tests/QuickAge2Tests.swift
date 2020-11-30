9739b284b1ccdda2e9acd976698f134762603c47
vitoyaokatsu <vitoyaokatsu@gmail.com>
2020-11-30 20:11:12 +0900

    Initial Commit

---
+++ QuickAge2Tests/QuickAge2Tests.swift
@@ -0,0 +1,33 @@
+//
+//  QuickAge2Tests.swift
+//  QuickAge2Tests
+//
+//  Created by Katsuhiko Yao on 2020/11/30.
+//
+
+import XCTest
+@testable import QuickAge2
+
+class QuickAge2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
